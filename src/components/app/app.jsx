a169ac398834ed33281745716f90949d6257a4b4
Alina <59539395+Malinka585@users.noreply.github.com>
2020-10-05 23:43:48 +0500

    Отправляет на проверку задание 7. Личный проект: маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import MainScreen from "../main-screen/main-screen";
-import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
+import MainScreen from "../main-screen/main-screen";
+import SignInScreen from "../sign-in-screen/sign-in-screen";
+import MyListScreen from "../my-list-screen/my-list-screen";
+import MoviePageScreen from "../movie-page-screen/movie-page-screen";
+import AddReviewScreen from "../add-review-screen/add-review-screen";
+import PlayerScreen from "../player-screen/player-screen";
+import PropTypes from "prop-types";
 
 
 const App = (props) => {
@@ -15,6 +20,21 @@ const App = (props) => {
           <MainScreen movieTitle={movieTitle}
             movieGenre={movieGenre}
             movieYear={movieYear}/>
+        </Route>
+        <Route exact path="/login">
+          <SignInScreen />
+        </Route>
+        <Route exact path="/mylist">
+          <MyListScreen />
+        </Route>
+        <Route exact path="/films/:id">
+          <MoviePageScreen />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReviewScreen />
+        </Route>
+        <Route exact path="/player/:id">
+          <PlayerScreen />
         </Route>
       </Switch>
     </BrowserRouter>
