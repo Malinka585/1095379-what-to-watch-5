523cfb1408b6b445c43852d5a5b67d4b32b81e82
Alina <59539395+Malinka585@users.noreply.github.com>
2020-11-25 00:47:47 +0500

    Отправляет задание на проверку 22. Личный проект: добро пожаловать, или посторонним вход воспрещён (часть 1)

---
+++ src/components/user-block/user-block.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {AuthorizationStatus} from "../../const";
+import {Link} from "react-router-dom";
+
+const UserBlock = (props) => {
+  const {authorizationStatus} = props;
+
+  return (
+    <div className="user-block">
+      {authorizationStatus === AuthorizationStatus.AUTH ?
+        <div className="user-block__avatar">
+          <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+        </div>
+        :
+        <Link to={`/login`} className="user-block__link">Sign in</Link>}
+    </div>
+  );
+};
+
+const mapStateToProps = ({USER}) => ({
+  authorizationStatus: USER.authorizationStatus,
+});
+
+UserBlock.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
+};
+
+export {UserBlock};
+export default connect(mapStateToProps)(UserBlock);
